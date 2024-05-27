6c9f66301c20bed16d56cfd5362f2cf5c49e8268
wally <goldenale5500@gmail.com>
2024-05-28 00:43:17 +0900

    기본 항목 추가 및 컴포지션 테스트

---
+++ simple-vue3/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h2>반응형 counter</h2>
+    <p>{{count}}</p>
+    <button @click="addCount">+</button>
+
+    <h2>노말 counter</h2>
+    <p>{{normalCount}}</p>
+    <button @click="addNormalCount">+</button>
+  </div>
+</template>
+
+<script>
+import {ref} from "vue";
+
+export default {
+  setup(){
+    // 반응형
+    const count = ref(0)
+    const addCount = () => {
+      count.value++
+    }
+
+    //노멀
+    let normalCount = 0
+    const addNormalCount = () => {
+      normalCount++
+    }
+
+    return {
+      count,
+      addCount,
+      normalCount,
+      addNormalCount
+    }
+  }
+}
+</script>
+
+
+
+<style scoped>
+
+</style>
