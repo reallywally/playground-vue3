d1e52b66352afc126592d67c1b60884bf54708ad
wally <goldenale5500@gmail.com>
2024-05-29 01:05:40 +0900

    reactive, ref 차이점
    - 어렵네

--- simple-vue3/src/App.vue
+++ simple-vue3/src/App.vue
@@ -1,43 +1,31 @@
 <template>
   <div>
-    <h2>반응형 counter</h2>
-    <p>{{count}}</p>
-    <button @click="addCount">+</button>
 
-    <h2>노말 counter</h2>
-    <p>{{normalCount}}</p>
-    <button @click="addNormalCount">+</button>
   </div>
 </template>
 
 <script>
-import {ref} from "vue";
+import {reactive, ref} from "vue";
 
 export default {
-  setup(){
-    // 반응형
+  setup() {
     const count = ref(0)
-    const addCount = () => {
-      count.value++
-    }
+    const state = reactive({
+      count,
+    })
 
-    //노멀
-    let normalCount = 0
-    const addNormalCount = () => {
-      normalCount++
-    }
+    count.value++
+    count.value++
+
+    console.log(count.value)
+    console.log(state.count)  // count 자체는 ref로 감싸져있지만 value로 접근하지 않아도 된다.
 
     return {
-      count,
-      addCount,
-      normalCount,
-      addNormalCount
+
     }
   }
 }
 </script>
-
-
 
 <style scoped>
 
